b584dd28efc7b5226d22a829c93b07fcc851adf9
Alexandra Mescheryakowa <sokolovamarina2525@gmail.com>
2024-05-31 11:57:54 +0300

    created a new code

---
+++ main.py
@@ -0,0 +1,21 @@
+import json
+from func import open_json_file, filter_list, sort_operation_list, hide_from, hide_to, date_converting
+
+# считывает файл
+def open_json_file():
+    """Считывает файл json"""
+    with open('operations.json', 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+
+data = open_json_file()
+
+# фильтрует, оставляя только выполненные операции, и сортирует их по дате
+data = sort_operation_list(filter_list(data))
+
+
+#производит вывод 5 последних операций
+for i in data[:5]:
+    print(f'{date_converting(i.get("date"))} {i.get("description")}')
+    print(f'{hide_from(i.get("from"))} -> {hide_to(i.get("to"))}')
+    print(f'{i["operationAmount"].get("amount")} {i["operationAmount"]["currency"].get("name")}\n')
